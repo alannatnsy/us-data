3deb3e2e990a501d91afcdd0aba56680c34b9ee2
Alanna Tanisya Anwar <96564861+alannatnsy@users.noreply.github.com>
2025-07-01 23:35:54 +0700

    fetch: get API

---
+++ src/api/dataService.js
@@ -0,0 +1,12 @@
+export const fetchPopulationData = async () => {
+  try {
+    const response = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const result = await response.json();
+    return result.data;
+  } catch (error) {
+    throw error;
+}
+};
